97a8154ca8a5538f0f14d51a69a04e510400a8b2
Tai Thanh Truong <tai.thanh.truong@esmartsystems.com>
2022-11-03 14:19:34 +0100

    fix: using mlDetectObject instead

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,25 @@
 import type { Frame } from 'react-native-vision-camera';
 
-export interface DetectedObject {
-  /**
-   * Frame location
-   */
-  left: number;
-  top: number;
-  right: number;
-  bottom: number;
-  /**
-   * TrackingID for an object
-   */
-  trackingID: number;
-}
-
-/**
- * Returns an array of matching `DetectedObject`s for the given frame.
- *
- */
-export function detectObjects(frame: Frame): DetectedObject[] {
+// export interface DetectedObject {
+//   /**
+//    * Frame location
+//    */
+//   left: number;
+//   top: number;
+//   right: number;
+//   bottom: number;
+//   /**
+//    * TrackingID for an object
+//    */
+//   trackingID: number;
+// }
+//
+// /**
+//  * Returns an array of matching `DetectedObject`s for the given frame.
+//  *
+//  */
+export function detectObjects(frame: Frame) {
   'worklet';
   // @ts-ignore
-  // eslint-disable-next-line no-undef
   return __detectObjects(frame);
 }
